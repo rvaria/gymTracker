9beaa727d1ec3d0e23d881cba074b27f8466ee43
Riddhi <ZHAC110@live.rhul.ac.uk>
2023-09-18 13:59:39 +0100

    Create routine basic functionality
    Added code to allow create routine button to open a new window

--- app/src/main/java/com/example/gymtracker/MainActivity.java
+++ app/src/main/java/com/example/gymtracker/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.gymtracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button createRoutine;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        createRoutine = findViewById(R.id.createRoutine);
+
+        createRoutine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent routine = new Intent(getApplicationContext(), CreateRoutineActivity.class);
+                startActivity(routine);
+            }
+        });
     }
 }
