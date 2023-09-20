b0bf156f1a750da9c292ab3292dd06800df627c7
Riddhi <ZHAC110@live.rhul.ac.uk>
2023-09-20 12:10:51 +0100

    Name routine feature
    Add alertdialog for user to input their workout routine name

--- app/src/main/java/com/example/gymtracker/MainActivity.java
+++ app/src/main/java/com/example/gymtracker/MainActivity.java
@@ -1,15 +1,20 @@
 package com.example.gymtracker;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button createRoutine;
+    private String dialogPrompt;
+    private EditText inputName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,12 +22,37 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         createRoutine = findViewById(R.id.createRoutine);
+        dialogPrompt = "Enter your workout routine name";
+        inputName = new EditText(this);
+
 
         createRoutine.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent routine = new Intent(getApplicationContext(), CreateRoutineActivity.class);
-                startActivity(routine);
+
+                AlertDialog.Builder createBox = new AlertDialog.Builder(MainActivity.this);
+                createBox.setTitle(dialogPrompt);
+                createBox.setView(inputName);
+
+                createBox.setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        System.out.println(inputName.getText().toString());
+                        Intent routine = new Intent(getApplicationContext(), CreateRoutineActivity.class);
+                        startActivity(routine);
+
+                    }
+                });
+
+                createBox.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                       dialog.dismiss();
+                    }
+                });
+
+                createBox.show();
+
             }
         });
     }
