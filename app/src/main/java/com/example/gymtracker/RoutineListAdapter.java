3f3cade9eb8ac9e3dbd9af8b1212666456249dcb
Riddhi <ZHAC110@live.rhul.ac.uk>
2023-12-03 18:40:12 +0000

    Create alert box
    Alert box to let user confirm they want to delete specific routine

--- app/src/main/java/com/example/gymtracker/RoutineListAdapter.java
+++ app/src/main/java/com/example/gymtracker/RoutineListAdapter.java
@@ -1,14 +1,19 @@
 package com.example.gymtracker;
 
 import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+
 import java.util.List;
 
 public class RoutineListAdapter extends ArrayAdapter<String> {
@@ -40,14 +45,36 @@ public class RoutineListAdapter extends ArrayAdapter<String> {
         deleteRoutine.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                exerciseDatabase = new ExerciseDatabase(getContext());
-                exerciseDatabase.deleteRoutine(item);
-                notifyDataSetChanged();
-
+                alertDelete(routineItems.get(position));
             }
         });
 
         return convertView;
+
+    }
+
+    public void alertDelete(String routineName) {
+
+        AlertDialog.Builder createBox = new AlertDialog.Builder(getContext());
+        createBox.setTitle("Are you sure you want to delete " + routineName + "?");
+
+        createBox.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                exerciseDatabase = new ExerciseDatabase(getContext());
+                exerciseDatabase.deleteRoutine(routineName);
+                notifyDataSetChanged();
+            }
+        });
+
+        createBox.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        createBox.show();
 
     }
 }
